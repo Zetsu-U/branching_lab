f01356d04676b283c4c4339c4e54a085637a0f80
Zetsu <kuimov06@mail.ru>
2025-12-10 22:46:36 +0500

    added 3 empty functions

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
+using std::string;
+using std::vector;
+
+
+vector<string> readLines(const string& path);
+
+void printLines(const vector<string>& lines);
+
+void writeLines(const vector<string>& lines, const string& path);
 
 int main() {
-    std::cout << "Hello world!\n";
-    return 0;
+    const string inputPath = "input.txt";
+    const string outputPath = "output.txt";
 
+    auto lines = readLines(inputPath);
+    printLines(lines);
+    writeLines(lines, outputPath);
+
+    std::cout << "Done.\n";
+    return 0;
+}
+
+
+vector<string> readLines(const string&) {
+    return {};
+}
+
+void printLines(const vector<string>&) {
+}
+
+void writeLines(const vector<string>&, const string&) {
 }
